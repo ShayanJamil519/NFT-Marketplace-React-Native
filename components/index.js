77616d150c42db68e846a141843ef9bae8488945
Shayan Jamil <shayanjamil500@gmail.com>
2022-12-27 10:34:26 +0500

    NFT Details Screen Designed

--- components/index.js
+++ components/index.js
@@ -2,21 +2,21 @@ import HomeHeader from "./HomeHeader";
 import NFTCard from "./NFTCard";
 import { RectButton, CircleButton } from "./Button";
 import { SubInfo, People, EndDate, EthPrice, NFTTitle } from "./SubInfo";
-// import DetailsBid from "./DetailsBid";
+import DetailsBid from "./DetailsBid";
 import FocusedStatusBar from "./FocusedStatusBar";
-// import DetailsDesc from './DetailsDesc';
+import DetailsDesc from "./DetailsDesc";
 
 export {
-  HomeHeader,
-  NFTCard,
-  //   SubInfo,
-  //   People,
-  //   EndDate,
-  RectButton,
-  CircleButton,
-  //   EthPrice,
-  //   NFTTitle,
-  //   DetailsBid,
-  FocusedStatusBar,
-  //   DetailsDesc,
+    HomeHeader,
+    NFTCard,
+    SubInfo,
+    People,
+    EndDate,
+    RectButton,
+    CircleButton,
+    EthPrice,
+    NFTTitle,
+    DetailsBid,
+    FocusedStatusBar,
+    DetailsDesc,
 };
